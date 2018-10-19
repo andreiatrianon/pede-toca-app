6f3e1d73ea0d69a002356f11756d4b881f6dfcd2
Andreia Trianon <andreiatrianon@outlook.com>
2018-10-18 22:27:40 -0300

    layout da seçao artistas, com popout sem botao para musicas

--- src/ArtistsCard.js
+++ src/ArtistsCard.js
@@ -4,47 +4,13 @@ import ArtistTrack from './ArtistTrack.js';
 import PropTypes from 'prop-types';
 import {Button} from 'react-materialize';
 
-class ArtistsCard extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      tracks: []
-    }
-    this.getArtistTracks = this.getArtistTracks.bind(this)
-  }
-
-  getArtistTracks(event) {
-    const BASE_URL = 'https://peaceful-badlands-98440.herokuapp.com'
-    const options = {
-      method: 'get',
-      credentials: 'include'
-    }
-  
-    fetch(`${BASE_URL}/artists/${this.props.id}/tracks`, options)
-      .then(res => res.json())
-      .then(data => {
-        if (data.length === 0) {
-          this.setState({tracks: [{title: 'Este artista não tem música'}]})  
-        } else {
-          this.setState({tracks: data})
-        }
-      })
-      
-  }
-
-  render() {
-    return ( 
-      <Card>
-        <h5>{this.props.name}</h5>
-        <p>{this.props.genre}</p>
-        <Button waves='light' className='purple accent-4 small' onClick={this.getArtistTracks}>músicas</Button>
-        <ul>
-          {this.state.tracks.map((track, index) => <ArtistTrack key={index} {...track} />)}
-        </ul>
-      </Card>
-    )
-  }
+function ArtistsCard(props) {
+  return ( 
+    <Card id={props.id}>
+      <h5>{props.name}</h5>
+      <p>{props.genre}</p>
+    </Card>
+  )
 }
 
 ArtistsCard.propTypes = {
--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Card.css';
+import ArtistTrack from './ArtistTrack.js';
 import {CollapsibleItem} from 'react-materialize';
 
-function Card(props) {
-  return (
-    <CollapsibleItem className='my-w-20 my-d-inline-block my-mb-10' header={props.children} icon='keyboard_voice'>teste</CollapsibleItem>
-  )
+class Card extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      tracks: []
+    }
+  }
+
+  componentDidMount() {
+    const BASE_URL = 'https://peaceful-badlands-98440.herokuapp.com'
+    const options = {
+      method: 'get',
+      credentials: 'include'
+    }
+
+    console.log(`${BASE_URL}/artists/${this.props.id}/tracks`);
+  
+    fetch(`${BASE_URL}/artists/${this.props.id}/tracks`, options)
+      .then(res => res.json())
+      .then(data => {
+        if (data.length === 0) {
+          this.setState({tracks: [{title: 'Este artista não tem música'}]})  
+        } else {
+          this.setState({tracks: data})
+        }
+      })
+  }
+
+  render() {
+    return (
+      <CollapsibleItem className='my-w-27 my-d-inline-block my-mb-10 purple accent-4' header={this.props.children} icon='keyboard_voice'>
+        <ul>
+          {this.state.tracks.map((track, index) => <ArtistTrack key={index} {...track} />)}
+        </ul>
+      </CollapsibleItem>
+    )
+  }
 }
 
 Card.propTypes = {
