3deadf816c3aa79bfdbc39e12d8e48dcc4b9acf2
Andreia Trianon <andreiatrianon@outlook.com>
2018-10-30 17:54:15 -0200

    icone para adicionar musica na playlist, inserindo no localStorage

--- src/TracksList.js
+++ src/TracksList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Col, Card} from 'react-materialize'
+import {Col, Card, Icon} from 'react-materialize'
 import './App.css'
 import './Card.css'
 
@@ -7,8 +7,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tracksList: []
+      tracksList: [],
+      playlist: []
     }
+    this.addTrackToPlaylist = this.addTrackToPlaylist.bind(this);
   }
 
   async componentDidMount() {
@@ -16,12 +18,28 @@ class App extends Component {
     return this.setState({tracksList: data})
   }
 
+  addTrackToPlaylist(event) {
+    let trackId = event.target.parentElement.getAttribute('data-id');
+    if (event.target.innerHTML === 'playlist_add') {
+      this.state.playlist.push(trackId);
+      localStorage.setItem('playlist', this.state.playlist);
+      event.target.textContent = 'playlist_add_check';
+      event.target.className = 'material-icons small icon-button my-green';
+    } else {
+      this.state.playlist = this.state.playlist.filter(el => el !== trackId);
+      localStorage.setItem('playlist', this.state.playlist);
+      event.target.textContent = 'playlist_add';
+      event.target.className = 'material-icons small icon-button my-orange';
+    }
+  }
+
   render() {
     return (
       <section id='tracks-list' className='white my-text-center my-p-2'>
         {this.state.tracksList.map((track, index) =>
-          <Col id={index} data-id={track.id} className='my-w-27 my-d-inline-block my-mr-1'>
+          <Col id={index} className='my-w-27 my-d-inline-block my-mr-1'>
             <Card className='my-bg-brown' title={track.title}>
+              <div data-id={track.id} onClick={this.addTrackToPlaylist}><Icon className='icon-button my-orange' small>playlist_add</Icon></div>
               <iframe src={track.url} />
             </Card>
           </Col>
